dd320c08044a7d6a3d52e3108b850f1d8ff00bc0
bichanna <nobu.bichanna@gmail.com>
2022-03-29 10:59:54 -0500

    Add some tests

--- test/millie_test.dart
+++ test/millie_test.dart
@@ -2,15 +2,34 @@ import 'package:millie/millie.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
+  group('Using Correct Suffix', () {
+    test('100', () => expect(millify(100), equals("100")));
+    test('thousand', () => expect(millify(1000), equals("1K")));
+    test('million', () => expect(millify(1000000), equals("1M")));
+    test('billion', () => expect(millify(1000000000), equals("1B")));
+    test('trillion', () => expect(millify(1000000000000), equals("1T")));
+  });
 
-    setUp(() {
-      // Additional setup goes here.
-    });
+  group("Working with Negative Numbers", () {
+    test('negative 100', () => expect(millify(-100), equals("-100")));
+    test('negative thousand', () => expect(millify(-1000), equals("-1K")));
+    test('negative million', () => expect(millify(-1000000), equals("-1M")));
+    test('negative billion', () => expect(millify(-1000000000), equals("-1B")));
+    test('negative trillion', () => expect(millify(-1000000000000), equals("-1T")));
+  });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
+  group("Converting to Lowercase", () {
+        test('100', () => expect(millify(100, lowercase: true), equals("100")));
+    test('thousand', () => expect(millify(1000, lowercase: true), equals("1k")));
+    test('million', () => expect(millify(1000000, lowercase: true), equals("1m")));
+    test('billion', () => expect(millify(1000000000, lowercase: true), equals("1b")));
+    test('trillion', () => expect(millify(1000000000000, lowercase: true), equals("1t")));
+  });
+
+  group("Adding An Additional Space", () {
+    test('thousand', () => expect(millify(1000, space: true), equals("1 K")));
+    test('million', () => expect(millify(1000000, space: true), equals("1 M")));
+    test('billion', () => expect(millify(1000000000, space: true), equals("1 B")));
+    test('trillion', () => expect(millify(1000000000000, space: true), equals("1 T")));
   });
 }
