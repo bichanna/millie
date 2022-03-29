cc5bf38f2002c13508abcb17edbf448c936132a8
bichanna <nobu.bichanna@gmail.com>
2022-03-29 11:20:01 -0500

    Add some tests

--- example/millie_example.dart
+++ example/millie_example.dart
@@ -1,6 +1,7 @@
 import 'package:millie/millie.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  print(millify(4200)); // prints 4.2K
+  print(millify(10000, lowercase: true)); // prints 10k
+  print(millify(1250000, space: true, lowercase: true)); // prints 1.3 m
 }
